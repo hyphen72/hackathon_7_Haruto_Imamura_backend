04cbcd65a71f39a98110b853e14e610f82406517
Haruto Imamura <imaharu4212@gmail.com>
2025-06-21 15:01:39 +0900

    add profile setting

--- hackathon/main.go
+++ hackathon/main.go
@@ -69,7 +69,7 @@ func generateUUID() string {
 }
 func userhandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT")
 	w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
 	switch r.Method {
 	case http.MethodOptions:
